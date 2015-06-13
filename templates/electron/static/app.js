79796ca880bb092eb5f3d800a8cbb76e8f5d80ce
clement <maboiteaspam@wanadoo.fr>
2015-06-13 20:42:07 +0800

    change demo

--- templates/electron/static/app.js
+++ templates/electron/static/app.js
@@ -6,4 +6,6 @@ document.write('The current version of node is ' + process.version );
 var fs = require('fs');
 
 var contents = fs.readFileSync(__dirname + '/../package.json', 'utf8');
-alert(contents);
+var pre = document.createElement('pre')
+pre.innerHTML = JSON.stringify(contents, null, 4)
+document.body.appendChild(pre)
